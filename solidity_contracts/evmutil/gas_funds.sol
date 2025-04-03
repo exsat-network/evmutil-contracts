9cc72d45dc2d4cac59e8b168a9f2c19a548f6b63
frank <frank@exsat.org>
2025-04-03 11:27:46 +0800

    feat:  new gas funds contract

---
+++ solidity_contracts/evmutil/gas_funds.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+// Gas Funds
+contract GasFunds  {
+
+    string  public linkedEOSAccountName;
+    address public linkedEOSAddress;
+    address public evmAddress;
+
+    constructor() {
+        linkedEOSAccountName = "gasfund.xsat";
+        linkedEOSAddress = 0xBBbbBBBBbBbBBBbBbBBbBbBb61B0Bd4D20EE0D90;
+        evmAddress = 0xBBbBbbbbbBbbbbBBBBbbbBbb56e40ee0D9000000;
+    }
+
+    function _isReservedAddress(address addr) internal pure returns (bool) {
+        return ((uint160(addr) & uint160(0xFffFfFffffFfFFffffFFFffF0000000000000000)) == uint160(0xBBbbBbBbbBbbBbbbBbbbBBbb0000000000000000));
+    }
+
+    function claim(address _target) external {
+        // The action is aynchronously viewed from EVM and looks UNSAFE.
+        // BUT in fact the call will be executed as inline action.
+        // If the cross chain call fail, the whole tx including the EVM action will be rejected.
+        bytes memory receiver_msg = abi.encodeWithSignature("claim(address,address)", _target, msg.sender);
+        (bool success, ) = evmAddress.call(abi.encodeWithSignature("bridgeMsgV0(string,bool,bytes)", linkedEOSAccountName, true, receiver_msg ));
+        if(!success) { revert(); }
+    }
+
+    function enfClaim(address _target) external {
+        // The action is aynchronously viewed from EVM and looks UNSAFE.
+        // BUT in fact the call will be executed as inline action.
+        // If the cross chain call fail, the whole tx including the EVM action will be rejected.
+        bytes memory receiver_msg = abi.encodeWithSignature("evmenfclaim(address)", msg.sender);
+        (bool success, ) = evmAddress.call(abi.encodeWithSignature("bridgeMsgV0(string,bool,bytes)", linkedEOSAccountName, true, receiver_msg ));
+        if(!success) { revert(); }
+    }
+
+    function ramsClaim(address _target, address _proxy) external {
+        // The action is aynchronously viewed from EVM and looks UNSAFE.
+        // BUT in fact the call will be executed as inline action.
+        // If the cross chain call fail, the whole tx including the EVM action will be rejected.
+        bytes memory receiver_msg = abi.encodeWithSignature("evmramsclaim(address)", msg.sender);
+        (bool success, ) = evmAddress.call(abi.encodeWithSignature("bridgeMsgV0(string,bool,bytes)", linkedEOSAccountName, true, receiver_msg ));
+        if(!success) { revert(); }
+    }
+}
